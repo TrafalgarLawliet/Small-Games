994b50bd4c4ff0fb3d868c71cb7b65afed932176
Lawliet <saurabh78963@gmail.com>
2021-02-28 15:32:16 +0530

    Created Array for "X" and "O"
    Added an array for storing the input by the users.

--- Tic-tac-toe.cpp
+++ Tic-tac-toe.cpp
@@ -2,17 +2,23 @@
 using namepsace std;
 int main()
 {
-  int a,b;
-  cin>>a>>b;
-  cout<<"     |       |       \n";
-  cout<<"     |       |       \n";
-  cout<<"     |       |       \n";
-  cout<<"_____ _______ ______ \n";
-  cout<<"     |       |       \n";
-  cout<<"     |       |       \n";
-  cout<<"     |       |       \n";
-  cout<<"_____ _______ ______ \n";
-  cout<<"     |       |       \n";
-  cout<<"     |       |       \n";
-  cout<<"     |       |       \n";
+  int i, j;
+  int c = 0;
+  int n = 9;
+  int a[n];
+  for (i = 1; i < 10; i++)
+    {
+      a[c] = i;
+      c++;
+      cout << i << " ";
+      if (i % 3 == 0)
+	{
+	  cout << "\n";
+	}
+    }
+  for (i = 0; i < 9; i++)
+    {
+      cout << "a[" << i + 1 << "]: " << a[i] << "\n";
+    }
+  return 0;
 }
