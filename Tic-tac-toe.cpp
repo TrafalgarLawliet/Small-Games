bc315c8619e294f76d57fdd96a90b5aa5d6c01b7
Lawliet <saurabh78963@gmail.com>
2021-03-06 12:43:30 +0530

    Added an index number for Input
    Added an Index number as "IndexValue" so that player can input the index value to mark "X" or "O".

--- Tic-tac-toe.cpp
+++ Tic-tac-toe.cpp
@@ -5,16 +5,18 @@ int
 main ()
 {
   int i, j;			//For Loop
+  int indexValue;
   //char cross = "X", zero = "O";
   int player = 1;		//For Input from Users
   char board[10] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
   i = 0;
   do
     {
-      player = (player % 2) ? 1 : 2;
-      cout << "Board: " << board[i] << "\tPlayer: " << player << "\n";
-      i++, player++;
+      player = (player % 2) ? 1 : 2;  //Selecting the Player
+      cout<<"Player "<<player<<" turn: ";
+      cin>>indexValue;
+      i++, player++;  //Updating the Board and Selecting the player
     }
-  while (i <= 10);
+  while (i <= 9);
   return 0;
 }
