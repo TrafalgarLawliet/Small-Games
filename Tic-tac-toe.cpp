5c46b5b5ef06a27e0cef9d4e7fc777ea39f75cb8
Lawliet <saurabh78963@gmail.com>
2021-03-06 15:52:05 +0530

    Use the inputs from users to fill Board
    Use the inputs taken from the users and fill the Board. Fill the Board and display it in the end.

--- Tic-tac-toe.cpp
+++ Tic-tac-toe.cpp
@@ -1,22 +1,40 @@
 #include <iostream>
 
 using namespace std;
-int
-main ()
+int main()
 {
-  int i, j;			//For Loop
+  int i, j; //For Loop
   int indexValue;
   //char cross = "X", zero = "O";
-  int player = 1;		//For Input from Users
-  char board[10] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+  int player = 1; //For Input from Users
+  char board[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
   i = 0;
   do
+  {
+    player = (player % 2) ? 1 : 2; //Selecting the Player
+    cout << "Player " << player << " turn: ";
+    cin >> indexValue;
+
+    if (board[indexValue] != 'X' || board[indexValue] != 'O')
     {
-      player = (player % 2) ? 1 : 2;  //Selecting the Player
-      cout<<"Player "<<player<<" turn: ";
-      cin>>indexValue;
-      i++, player++;  //Updating the Board and Selecting the player
+      if (player == 1)
+      {
+        board[indexValue] = 'X';
+      }
+      else if (player == 2)
+      {
+        board[indexValue] = 'O';
+      }
     }
-  while (i <= 9);
+    i++, player++; //Updating the Board and Selecting the player
+  } while (i < 9);
+
+  cout << endl;
+
+  cout<<"BOARD\n";
+  cout<<board[1]<<" "<<board[2]<<" "<<board[3]<<"\n";
+  cout<<board[5]<<" "<<board[5]<<" "<<board[6]<<"\n";
+  cout<<board[7]<<" "<<board[8]<<" "<<board[9]<<"\n";
+
   return 0;
 }
