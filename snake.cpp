4f8756d1fd12e65b1fb9d66fbad83173b8bdf7af
Lawliet <saurabh78963@gmail.com>
2021-03-07 20:44:13 +0530

    Graphics header file added
    header file added for the implementation of the graphics required in the Snake Game.

--- snake.cpp
+++ snake.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <graphics>
 using namespace std;
 
 int main()
 {
+    int gd = DETECT, gm;
+    initgraph(&gd, &gm, "C:\MinGW\lib");
+    rectangle(10, 10, 10, 150);
+    getch();
+    closegraph();
     return 0;
 }
